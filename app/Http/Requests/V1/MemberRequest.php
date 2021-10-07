a4ec7b7bd56ea146d7a90f4c8d002673e7364e73
Saqlain Khalid <saqlain@integrity.pk>
2021-10-07 12:46:25 +0500

    create request validation

--- app/Http/Requests/V1/MemberRequest.php
+++ app/Http/Requests/V1/MemberRequest.php
@@ -4,6 +4,10 @@ namespace App\Http\Requests\V1;
 
 use Pearl\RequestValidate\RequestAbstract;
 
+use App\Http\Models\User;
+
+use Illuminate\Validation\Rule;
+
 class MemberRequest extends RequestAbstract
 {
     /**
@@ -24,7 +28,14 @@ class MemberRequest extends RequestAbstract
     public function rules(): array
     {
         return [
-            //
+            'first_name' => 'required|alpha|max:100',
+            'last_name' => 'required|alpha|max:100',
+            'city' => 'string|required',
+            'country' => 'string|required',
+            'phone' =>  ($this->isMethod('put')) ? 'nullable|numeric|max:999999999999999999999999|unique:users,phone,'. $this->id : 'nullable|numeric|max:999999999999999999999999|unique:users,phone',
+            'email' => ($this->isMethod('put')) ? 'required|email:rfc,dns|unique:users,email,' . $this->id : 'required|email:rfc,dns|unique:users,email',
+            'password' => ($this->isMethod('post')) ? 'required|confirmed|min:6||max:100|string' : '',
+            'status' => 'required|string|'. Rule::in(array_keys(User::STATUS)),
         ];
     }
 
